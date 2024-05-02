2d2b075cfb3658d03cf0ab4aa0dbd387a1f2f104
Markus Neufeld <uwlhp@student.kit.edu>
2024-05-02 20:29:51 +0200

    disallow moveing plasmashell LOL

--- move-window-to-activity.sh
+++ move-window-to-activity.sh
@@ -7,6 +7,16 @@ fi
 
 activity_name="$1"
 
+window_id=$(xdotool getwindowfocus)
+
+window_info=$(xprop -id $window_id)
+
+# Check if the current window is Plasmashell
+if echo "$window_info" | grep -q "WM_CLASS(STRING) = \"plasmashell\", \"plasmashell\""; then
+    echo "This is the Plasmashell. It should not be moved to another activity."
+    exit 2
+fi
+
 output=$(qdbus --literal org.kde.ActivityManager /ActivityManager/Activities ListActivitiesWithInformation)
 
 activity_id=$(echo "$output" | grep -oP "(?<=\[Argument: \(ssssi\) \")[^\"]+(?=\", \"$activity_name\")")
@@ -16,6 +26,6 @@ if [ -z "$activity_id" ]; then
     exit 2
 else
     echo "$activity_id"
-    xprop -f _KDE_NET_WM_ACTIVITIES 8s -id $(xdotool getwindowfocus) -set _KDE_NET_WM_ACTIVITIES $activity_id
+    xprop -f _KDE_NET_WM_ACTIVITIES 8s -id $window_id -set _KDE_NET_WM_ACTIVITIES $activity_id
     exit 0
 fi
