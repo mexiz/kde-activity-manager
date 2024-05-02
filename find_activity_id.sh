08e407cbea4c0534ac6f5679febfe05867294d06
Markus Neufeld <uwlhp@student.kit.edu>
2024-05-02 11:04:37 +0200

    skript

---
+++ find_activity_id.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Please provide the name of the activity as an argument."
+    exit 1
+fi
+
+activity_name="$1"
+
+output=$(qdbus --literal org.kde.ActivityManager /ActivityManager/Activities ListActivitiesWithInformation)
+
+activity_id=$(echo "$output" | grep -oP "(?<=\[Argument: \(ssssi\) \")[^\"]+(?=\", \"$activity_name\")")
+
+if [ -z "$activity_id" ]; then
+    echo "Error: Activity not found."
+    exit 2
+else
+    echo "$activity_id"
+    exit 0
+fi
